8bd43a646fe0060cdac7d13915ecc369a85ad1fd
reycastillon-ubidy <rey.castillon@ubidy.com>
2024-07-22 23:00:04 +0800

    update code and added foreach iterator

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-let fruits = ["apple","banana","grapes"];
+let fruits = ["apple", "banana", "grapes"];
 
 for (let i = 0; i < fruits.length; i++) {
-    const element = fruits[i];
-    console.log(`I like ${element}`)
+  const element = fruits[i];
+  console.log(`I like ${element}`);
 }
 
+let favFruit = fruits.forEach((fruit) => {
+  console.log(`My favorite fruit is ${fruit}`);
+});
